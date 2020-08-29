68a43891315f670fbfc358b8af7e8327631fff5a
Michael Spremulli <michaels.wpi@gmail.com>
2020-08-29 12:07:18 -0400

    calculated mortgage payment schedule

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -18,22 +18,34 @@ public class MortgageCalculator {
         return scanner.nextFloat();
     }
 
-    public static void printReport(float total, float rate, float years) {
-        double monthlyPayment = calculateMonthlyPayment(total, rate, years);
+    public static void printReport(float mortgageTotal, float interestRate, float numberOfYears) {
+        double monthlyPayment = calculateMonthlyPayment(mortgageTotal, interestRate, numberOfYears);
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + monthlyPayment);
     }
 
-    public  static void printPaymentSchedule(float total, float rate, float years){
+    public  static void printPaymentSchedule(float mortgageTotal, float interestRate, float numberOfYears){
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
-        System.out.println();
+        for(int month = 1; month < numberOfYears * 12; month++) {
+            System.out.println(Math.round(calculatePaymentSchedule(mortgageTotal, interestRate, numberOfYears, month)));
+        }
     }
 
-    public static double calculateMonthlyPayment(float total, float interest, float months){
-        float monthlyInterest = interest / 12;
-        return total *
+    public static double calculatePaymentSchedule(float mortgageTotal, float interestRate, float numberOfYears, float numberOfPaymentsMade){
+        float monthlyInterest = interestRate / 12;
+        float numberOfPayments = numberOfYears * 12;
+
+        double balance = mortgageTotal *
+                (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        return balance;
+    }
+
+    public static double calculateMonthlyPayment(float mortgageTotal, float interestRate, float months){
+        float monthlyInterest = interestRate / 12;
+        return mortgageTotal *
               ( monthlyInterest * Math.pow(1 + monthlyInterest, months))
               / (Math.pow(1 + monthlyInterest, months) - 1);
     }
