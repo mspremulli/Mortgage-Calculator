4b3630bd3f5e339a2dc63f694b4224e3ebb90152
Michael Spremulli <michaels.wpi@gmail.com>
2020-10-04 22:48:42 -0400

    cleaned up code

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,34 +1,33 @@
-import java.util.Scanner;
-
 public class Calculator {
     final static int MONTHS_IN_A_YEAR = 12;
     final static int _100_PERCENT = 100;
 
-    public int mortgageTotal;
+    public int principle;
     public float interestRate;
     public int numberOfYears;
 
     public Calculator(int mortgageTotal, float interestRate, int numberOfYears) {
-        this.mortgageTotal = mortgageTotal;
+        this.principle = mortgageTotal;
         this.interestRate = interestRate;
         this.numberOfYears = numberOfYears;
     }
 
     public double calculatePaymentSchedule(int numberOfPaymentsMade ) {
-        float monthlyInterest =  interestRate / MONTHS_IN_A_YEAR;
+        float monthlyInterest =  1 + interestRate / MONTHS_IN_A_YEAR;
         float numberOfPayments = numberOfYears * MONTHS_IN_A_YEAR;
 
-        return mortgageTotal *
-                (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return principle *
+                (Math.pow(monthlyInterest, numberOfPayments) - Math.pow(monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(monthlyInterest, numberOfPayments) - 1);
     }
 
     public double calculateMonthlyPayment() {
-        float monthlyInterest = interestRate / MONTHS_IN_A_YEAR;
-        int months = numberOfYears * 12;
-        return mortgageTotal *
-                ( monthlyInterest * Math.pow(1 + monthlyInterest, months))
-                / (Math.pow(1 + monthlyInterest, months) - 1);
+        float monthlyInterest = 1 + interestRate / MONTHS_IN_A_YEAR;
+        int months = numberOfYears * MONTHS_IN_A_YEAR;
+        return principle *
+                ( monthlyInterest * Math.pow(monthlyInterest, months))
+                / (Math.pow(monthlyInterest, months) - 1);
     }
 
 }
